8c6d3c708655ed7bd2b1a22a9cd647ffb939f58d
v1siuol <lvzh.louis2013@gmail.com>
2018-12-04 19:49:46 -0500

    Add files close; Add quickstart sup
    - [A] ignore virtual env
    - [A] add pip requirements
    - [F] close files after
    - [M] add quickstart readme

--- subprocess_test.py
+++ subprocess_test.py
@@ -1,6 +1,8 @@
 import subprocess
 
-f = open('bunny.gts', 'r')
-o = open('bunny_smooth.gts', 'w')
-subprocess.run(["./smoother", "1", "1", "1"], stdin=f, stdout=o)
+# f = open('bunny.gts', 'r')
+# o = open('bunny_smooth.gts', 'w')
+with open('bunny.gts', 'r') as f:
+	with open('bunny_smooth.gts', 'w') as o:
+		subprocess.run(["./smoother", "1", "1", "1"], stdin=f, stdout=o)
 
