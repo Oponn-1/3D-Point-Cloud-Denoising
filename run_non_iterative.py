46af92cb4c9d1792247897f63d003123ffa29f29
Andres De La Fuente Duran <expendablebin@gmail.com>
2018-12-05 06:46:11 -0500

    Fixed input reads

--- run_non_iterative.py
+++ run_non_iterative.py
@@ -7,17 +7,20 @@ from cloud_to_gts import gts_write
 
 def run_non_iterative():
     
+    print()
     print("Running Non-Iterative, Feature Preserving Mesh Smoothing...")
 
-    arg1 = input("sigma_f: ")
-    arg2 = input("sigma_g: ")
+    arg1 = int(input("sigma_f: "))
+    arg2 = int(input("sigma_g: "))
 
     print("Probability distribution options:")
     print("1: Gaussian")
     print("2: Exponential")
     print("3: Gamma")
-    dist_mode = input("selection: ")
+    dist_mode = int(input("selection: "))
 
     tri, points = input_triangulation()
     gts_write(tri, points)
 
+
+run_non_iterative()
