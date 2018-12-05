2ef73ca0923e94de87d55fbf3c28b7cc9bc6d558
Andres De La Fuente Duran <expendablebin@gmail.com>
2018-12-05 06:45:15 -0500

    Added input lines for filenames

--- subprocess_test.py
+++ subprocess_test.py
@@ -1,6 +1,9 @@
 import subprocess
 
-with open('bunny.gts', 'r') as f:
-	with open('bunny_smooth.gts', 'w') as o:
+in_file = input("Input GTS: ")
+out_file = input("Output GTS: ")
+
+with open(in_file, 'r') as f:
+	with open(out_file, 'w') as o:
 		subprocess.run(["./smoother", "1", "1", "1"], stdin=f, stdout=o)
 
